7ef6f14361a20e3e601cc6889f93f8b6063b93d1
PatriciaPrado <pacdoprado@gmail.com>
2019-08-02 22:28:07 -0300

    AgendaSeries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserForm from './Pages/User/Form';
-import UserList from './Pages/User/List';
-import Input from './Components/Input/Input';
-import Modal from './Components/Modal/Modal';
+import Selected from './components/Selected/Selected'
+import Input from './components/Input/Input';
+// import UserForm from './pages/User/Form';
+// import UserList from './pages/User/List';
+// import Modal from './components/Modal/Modal';
 
 
 function App() {
   return (
     <div className="App">
+    <Input
+      id="nome"
+      initialValue="Miranda"
+      label="Nome"
+      />
+      <Selected
+      label="Estado"
+      list="{["SP", "RJ", "MG"]}"
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
